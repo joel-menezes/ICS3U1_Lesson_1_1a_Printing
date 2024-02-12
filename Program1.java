1401de84120b3f98b778a0252bc045bcaeb21d39
joel-menezes <joel.menezes25@ycdsbk12.ca>
2024-02-12 09:27:42 -0500

    Random edits for CCC 96 S2 for some reason

--- Program1.java
+++ Program1.java
@@ -7,7 +7,32 @@ class Program1 extends ConsoleProgram {
   
   public void run() {
     // your code goes here
+
     
-    
+    int intRead = readInt("");
+    int[] intNums = {readInt("")};
+    for (int i = 0; i < intRead; i++){
+
+    }
+
+    int intSave = intRead;
+    String strEnd = "";
+    String strCont = "";
+    System.out.println(intRead);
+    while (intSave > 11){
+      String blah = ""+intSave;
+      strEnd = (String) blah.substring(blah.length() - 1);
+      strCont = (String) blah.substring(0,blah.length() - 1);
+      intSave = Integer.parseInt(strCont) - Integer.parseInt(strEnd) ;
+      System.out.println(intSave);
+      
+
+    }
+    if (intSave == 11){
+      System.out.println("The number "+intRead+" is divisible by 11.");
+    }
+    else if (intSave < 11){
+      System.out.println("The number "+intRead+" is not divisible by 11.");
+    }
   }
 }
